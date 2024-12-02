8feccbe6e708fb0662eecc3815cde82c2d0013cc
amitrahman1026 <amit.amit.rahman@gmail.com>
2024-12-03 00:56:40 +0800

    update algo template path

---
+++ cp_templates/template.cpp
@@ -0,0 +1,24 @@
+// Problem: $(PROBLEM)
+// Contest: $(CONTEST)
+// URL: $(URL)
+// Memory Limit: $(MEMLIM)
+// Time Limit: $(TIMELIM)
+// Start: $(DATE)
+
+#include <iostream>
+
+void solve() {
+    // Add your solution here
+}
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cout.tie(NULL);
+    int tc = 1;
+    // cin >> tc;
+    for (int t = 1; t <= tc; t++) {
+        // cout << "Case #" << t << ": ";
+        solve();
+    }
+}
